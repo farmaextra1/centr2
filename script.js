edefe5c3f8ef9c15e1f45fa3a62fa58c788d9d85
farmaextra1 <lorencsam@seznam.cz>
2021-03-23 18:06:30 +0100

    vetsi-zmena

--- script.js
+++ script.js
@@ -37,3 +37,37 @@ $( document ).ready(function() {
     $(".slick-prev").html("");
 });
 /*KONEC DRUHYHO SLIDERU JS */
+
+function platba1()
+{
+    document.getElementById('platbicka2').style.backgroundColor= "white";
+    document.getElementById('platbicka2').style.color= "black";
+    document.getElementById('platbicka1').style.backgroundColor= "rgb(54, 54, 54)";
+    document.getElementById('platbicka1').style.color= "white";
+    document.getElementById('cenaZmena').style.color= "rgb(86, 93, 161)";
+    document.getElementById('cenaZmena2').style.color= "rgb(86, 93, 161)";
+    document.getElementById('platbicka3').style.backgroundColor= "white";
+    document.getElementById('platbicka3').style.color= "black";
+}
+function platba2()
+{
+    document.getElementById('platbicka1').style.backgroundColor= "white";
+    document.getElementById('platbicka1').style.color= "black";
+    document.getElementById('platbicka2').style.backgroundColor= "rgb(54, 54, 54)";
+    document.getElementById('platbicka2').style.color= "white";
+    document.getElementById('cenaZmena').style.color= "rgb(255, 237, 137)";
+    document.getElementById('cenaZmena2').style.color= "rgb(255, 237, 137)";
+    document.getElementById('platbicka3').style.backgroundColor= "white";
+    document.getElementById('platbicka3').style.color= "black";
+}
+function platba3()
+{
+    document.getElementById('platbicka1').style.backgroundColor= "white";
+    document.getElementById('platbicka1').style.color= "black";
+    document.getElementById('platbicka3').style.backgroundColor= "rgb(54, 54, 54)";
+    document.getElementById('platbicka3').style.color= "white";
+    document.getElementById('cenaZmena').style.color= "rgb(86, 93, 161)";
+    document.getElementById('cenaZmena2').style.color= "rgb(86, 93, 161)";
+    document.getElementById('platbicka2').style.backgroundColor= "white";
+    document.getElementById('platbicka2').style.color= "black";
+}
